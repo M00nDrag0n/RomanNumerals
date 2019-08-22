d8169bc2d593819081d5ba049a1c49a02a8e9430
Student <example@example.com>
2019-08-22 13:23:00 -0700

    save EVERY syntax error. LEAVE NO ERROR BEHIND. THere is no cure.

--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,22 @@ var roNum = function(numeral){
       return "II";
   }else if(numeral === 3)
       return "III";
+  }else if(numeral === 4)
+      return "IV";
+  }else if(numeral === 5)
+      return "V";
+  }else if(numeral === 6)
+      return "VI";
+  }else if(numeral === 7)
+      return "VII";
+  }else if(numeral === 8)
+      return "VIII";
+  }else if(numeral === 9)
+        return "VIV";
+  }else if(numeral === 10)
+        return "X";
+    else return "Sorry, this only goes up to 10."}
+  }}}}}}
 
 
 
